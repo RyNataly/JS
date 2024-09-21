17e5c0b58b8040238e61cccd117fbaf9742a879c
Natalia Ryzhenko <ryzhena@gmail.com>
2024-09-21 22:47:41 +0300

    Выполнение первого задания

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = "Мое первое ДЗ";
+let screens = 4;
+let screenPrice;
+let rollback;
+let fullPrice;
+const adaptive = true;
+
+alert("Привет, это мое первое задание");
+console.log("А тут все видно!");
