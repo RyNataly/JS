fb3c307f763106833ff45cf21f4d06cc538b9f03
Natalia Ryzhenko <ryzhena@gmail.com>
2024-09-22 14:38:15 +0300

    Выполнение второго задания

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-const title = "Мое первое ДЗ";
-let screens = 4;
-let screenPrice;
-let rollback;
-let fullPrice;
-const adaptive = true;
+ title = "Мой первый проект"
+ screens = "Простые, Сложные, Интерактивные"
+ screenPrice = 25
+ rollback = Math.random() * 100
+ fullPrice = 2546783000
+ adaptive = true
 
-alert("Привет, это мое первое задание");
-console.log("А тут все видно!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+// Вывести в консоль длину строки из переменной screens
+console.log("Длина строки " + screens.length);
+
+// Вывести в консоль “Стоимость верстки экранов  (screenPrice) рублей/ долларов/гривен/юани” и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " долларов");
+
+// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+console.log((screens.toLowerCase()).split(", "));
+
+// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
+console.log("Процент отката посреднику за работу " + Math.round(fullPrice * (rollback/100)) + "%");
