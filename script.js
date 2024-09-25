9c85bb3df96e537925cb81291236bda1f0db3f06
Natalia Ryzhenko <ryzhena@gmail.com>
2024-09-25 21:54:04 +0300

    4 - функции

--- script.js
+++ script.js
@@ -1,49 +1,60 @@
- 'use strict';
+'use strict';
 
- let title = prompt("Как называется ваш проект?");
- let screens = prompt("Какие типы экранов нужно разработать?"); //"Простые, Сложные, Интерактивные"
- let screenPrice = +prompt("Сколько будет стоить данная работа?", "12000"); 
- let adaptive = confirm("Нужен ли адаптив на сайте?");// == "Да" ? true : false; 
- console.log(adaptive);
- let service1 = prompt("Какой дополнительный тип услуги нужен?");
- let servicePrice1 = +prompt("Сколько это будет стоить?", "10000");
- let service2 = prompt("Какой дополнительный тип услуги нужен?");
- let servicePrice2 = +prompt("Сколько это будет стоить?", "10000");
- let fullPrice = screenPrice + servicePrice1 + servicePrice2;
- let rollback = Math.random() * 100;
- let percent = Math.round(fullPrice * (rollback/100));
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?"); //"Простые, Сложные, Интерактивные"
+let screenPrice = +prompt("Сколько будет стоить данная работа?", "12000"); 
+let adaptive = confirm("Нужен ли адаптив на сайте?");// == "Да" ? true : false; 
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?", "10000");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?", "10000");
+let rollback = 10;
+let servicePercentPrice, allServicePrices, fullPrice;
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log("Длина строки " + screens.length);
-console.log("Стоимость верстки экранов " + screenPrice + " рублей");
-console.log("Стоимость разработки сайта " + fullPrice + " долларов");
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+ }
 
-console.log((screens.toLowerCase()).split(", "));
-
-console.log("Процент отката посреднику за работу " + percent);
-
-let servicePercentPrice = fullPrice - percent;
-console.log("Итоговая стоимость за вычетом отката посреднику " + servicePercentPrice);
-
-if (fullPrice >= 30000) console.log("Даем скидку в 10%");
-if (fullPrice >= 15000 && fullPrice < 30000) console.log("Даем скидку в 5%");
-if (fullPrice > 0 && fullPrice < 15000) console.log("Скидка не предусмотрена");
-if (fullPrice <= 0) console.log("Что то пошло не так");
-
-//Другой вариант
-switch (true) {
-  case fullPrice >= 30000: 
-    console.log("Даем скидку в 10%");
-    break
-  case fullPrice >= 15000 && fullPrice < 30000:
-    console.log("Даем скидку в 5%");
-    break
-  case fullPrice > 0 && fullPrice < 15000:
-    console.log("Скидка не предусмотрена");
-    break
-  case fullPrice <= 0:
-    console.log("Что то пошло не так");
+// расчет скидки
+const getRollbackMessage = function (price) { 
+  if (price >= 30000) 
+      return "Даем скидку в 10%"
+  else if (price >= 15000 && fullPrice < 30000)
+      return "Даем скидку в 5%"
+  else if (price > 0 && fullPrice < 15000)
+      return "Скидка не предусмотрена"
+  if (price <= 0) 
+      return "Что то пошло не так"
 }
 
+// сумма всех дополнительных услуг
+const getAllServicePrices = function (price1, price2) { 
+  return price1 + price1;  
+}
+
+// сумма стоимости верстки и стоимости дополнительных услуг
+function getFullPrice (price1, price2) { 
+  return price1 + price1;   
+}
+
+const getTitle = function (title) { 
+  return title = title[0].toUpperCase() + title.slice(1).toLowerCase();
+}
+
+// итоговая стоимость за вычетом процента отката
+const getServicePercentPrices = function (price, rollback) { 
+  return price - Math.round(fullPrice * (rollback/100));  
+}
+
+allServicePrices = getAllServicePrices(servicePrice1, servicePrice2); 
+fullPrice = getFullPrice (screenPrice, allServicePrices);
+servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
+title = getTitle(title);
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
+console.log((screens.toLowerCase()).split(", "));
+console.log(getRollbackMessage(fullPrice));
+console.log("Итоговая стоимость за вычетом отката посреднику " + servicePercentPrice);
