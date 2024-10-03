68dbbb5e7ab298ad6da6dbd7b8b07fece97e471b
Natalia Ryzhenko <ryzhena@gmail.com>
2024-10-03 20:27:21 +0300

    10 - работа с элементами на странице - испр

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
 'use strict'
 
-const Books = document.querySelectorAll('.book') 
-Books[0].before(Books[1])
-Books[5].after(Books[2])
-Books[3].before(Books[4])
-
-
+const books = document.querySelectorAll('.book') 
 const bgImage = document.body
+const book4 = books[4].children[0].children[0] 
+const adv = document.querySelector('.adv')
+const book2 = books[0].querySelectorAll('li') 
+const book5 = books[5].querySelectorAll('li') 
+const ulBook6 = books[2].querySelectorAll('ul')
+const book6List = books[2].querySelectorAll('li')
+const book6 = books[2].querySelectorAll('li')[2].cloneNode(true)
+
+
+books[0].before(books[1])
+books[5].after(books[2])
+books[3].before(books[4])
+
 bgImage.style.backgroundImage = "url('./image/adv.jpg')"
 
-const Book4 = Books[4].children[0].children[0]   //Почему то через Books[4].h2  не работает, почему?
-Book4.textContent = "\n            Книга 3. this и Прототипы Объектов"
+book4.textContent = "\n            Книга 3. this и Прототипы Объектов"
 
-const adv = document.querySelector('.adv')
 adv.remove()
 
-const Book2 = Books[0].querySelectorAll('li') 
-Book2[4].before(Book2[6])
-Book2[4].before(Book2[8])
+book2[4].before(book2[6])
+book2[4].before(book2[8])
+book2[10].before(book2[2])
+ 
+book5[2].before(book5[9])
+book5[4].after(book5[2])
+book5[7].after(book5[5])
 
-const Book5 = Books[5].querySelectorAll('li') 
-Book5[2].before(Book5[9])
-Book5[4].after(Book5[2])
+book6.textContent = "Глава 8: За пределами ES6"
+ulBook6[0].append(book6)
+book6List[8].insertAdjacentElement("beforeEnd", book6)
 
-const ulBook6 = Books[2].querySelectorAll('ul')
-const Book6List = Books[2].querySelectorAll('li')
-const Book6 = Books[2].querySelectorAll('li')[2].cloneNode(true)
-Book6.textContent = "Глава 8: За пределами ES6"
-ulBook6[0].append(Book6)
-// Book6List[9].before(Book6List[10])
-Book6List[8].insertAdjacentElement("beforeEnd", Book6)
-
-console.log(Book6List)
-console.log(Book6)
+console.log(book6List)
+console.log(book6)
