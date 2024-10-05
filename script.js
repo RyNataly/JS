adf2341135f1d972bee1c50fdf3205d2c3cf7fe7
Natalia Ryzhenko <ryzhena@gmail.com>
2024-10-05 15:23:59 +0300

    11 - события

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
-'use strict'
+ 'use strict'
 
-const books = document.querySelectorAll('.book') 
-const bgImage = document.body
-const book4 = books[4].children[0].children[0] 
-const adv = document.querySelector('.adv')
-const book2 = books[0].querySelectorAll('li') 
-const book5 = books[5].querySelectorAll('li') 
-const ulBook6 = books[2].querySelectorAll('ul')
-const book6List = books[2].querySelectorAll('li')
-const book6 = books[2].querySelectorAll('li')[2].cloneNode(true)
+const btn = document.getElementById('btn')
+const e_btn = document.getElementById('e_btn')
+const inputText = document.getElementById('text')
+const square = document.getElementById('square')
 
+const rangeClick = function(event) {
+    document.getElementById('circle').style.height = event.target.value + "%"
+    document.getElementById('circle').style.width = event.target.value + "%"
+    document.getElementById('range-span').textContent = event.target.value
+}
 
-books[0].before(books[1])
-books[5].after(books[2])
-books[3].before(books[4])
+const btnClick = function() {
+   if (inputText.value != '') {
+      square.style.backgroundColor = inputText.value
+   }else 
+      alert ("Введите название цвета в строку ввода!")
+}
 
-bgImage.style.backgroundImage = "url('./image/adv.jpg')"
+// inputText.addEventListener('change', squareColor)
+btn.onclick = btnClick
+e_btn.style.display = "none";
+document.getElementById('range').addEventListener('change', rangeClick)
 
-book4.textContent = "\n            Книга 3. this и Прототипы Объектов"
-
-adv.remove()
-
-book2[4].before(book2[6])
-book2[4].before(book2[8])
-book2[10].before(book2[2])
- 
-book5[2].before(book5[9])
-book5[4].after(book5[2])
-book5[7].after(book5[5])
-
-book6.textContent = "Глава 8: За пределами ES6"
-ulBook6[0].append(book6)
-book6List[8].insertAdjacentElement("beforeEnd", book6)
-
-console.log(book6List)
-console.log(book6)
+console.dir(inputText)
+console.dir(document.getElementById('circle').style)
